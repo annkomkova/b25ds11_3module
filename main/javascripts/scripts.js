d23438dc33e230c436d1e0102f51362dd355cc23
Anna Komkova <ann.komkova98@gmail.com>
2026-03-02 13:17:07 +0300

    js anim

--- main/javascripts/scripts.js
+++ main/javascripts/scripts.js
@@ -1,6 +1,118 @@
 flipCard()
 moveProgressBar()
 drawSVG()
+getRandomImage()
+changeImgByCursor()
+animatePopup()
+moveCursor()
+
+function moveCursor() {
+  let cursor = document.querySelector('.cursor')
+
+  document.addEventListener('mousemove', (event) => {
+    let x = event.pageX
+    let y = event.pageY
+
+    cursor.style.top = `${y}px`
+    cursor.style.left = `${x}px`
+  })
+}
+
+function animatePopup() {
+  let button = document.querySelector('.randomCoordsButton')
+  let popup = document.querySelector('.popup')
+  let section = document.querySelector('.header')
+  let container = document.querySelector('.popupContainer')
+
+  let sectionW = section.getBoundingClientRect().width
+  let sectionH = section.getBoundingClientRect().height
+
+  let containerW = container.getBoundingClientRect().width
+  let containerH = container.getBoundingClientRect().height
+
+  let widthRange = sectionW - containerW
+  let heightRange = sectionH - containerH
+
+  button.addEventListener('click', () => {
+    console.log(containerW, containerH)
+
+    //анимация "убегающей" кнопки
+    container.style.position = 'absolute'
+    container.style.top = `${Math.random() * heightRange}px`
+    container.style.left = `${Math.random() * widthRange}px`
+
+    //анимация попапа
+    let popupEl = document.createElement('div')
+    popupEl.classList.add('popupEl')
+    popupEl.innerText = '🤍'
+
+    popup.appendChild(popupEl)
+
+    setTimeout(() => {
+      popupEl.remove()
+    }, 2000)
+  })
+}
+
+function changeImgByCursor() {
+  let coordX = document.querySelector('.coordX')
+  let coordY = document.querySelector('.coordY')
+  let viewport = document.querySelector('.viewport')
+  let box = document.querySelector('.imgBox')
+
+  let img0 = document.querySelector('.blickImg0')
+  let img1 = document.querySelector('.blickImg1')
+  let img2 = document.querySelector('.blickImg2')
+  let img3 = document.querySelector('.blickImg3')
+
+  document.addEventListener('mousemove', (event) => {
+    let cursorX = event.pageX
+    let cursorY = event.pageY
+
+    coordX.innerText = `cursor X: ${cursorX}`
+    coordY.innerText = `cursor Y: ${cursorY}`
+
+    let vieportW = document.documentElement.clientWidth
+    let vieportH = document.documentElement.clientHeight
+    viewport.innerText = `width: ${vieportW}px; height: ${vieportH}px`
+
+    let centerX = parseInt(vieportW / 2)
+    let centerY = parseInt(vieportH / 2)
+
+    if (cursorX < centerX && cursorY < centerY) {
+      img0.style.opacity = '1'
+    } else {
+      img0.style.opacity = '0'
+    }
+    if (cursorX > centerX && cursorY < centerY) {
+      img1.style.opacity = '1'
+    } else {
+      img1.style.opacity = '0'
+    }
+    if (cursorX > centerX && cursorY > centerY) {
+      img2.style.opacity = '1'
+    } else {
+      img2.style.opacity = '0'
+    }
+    if (cursorX < centerX && cursorY > centerY) {
+      img3.style.opacity = '1'
+    } else {
+      img3.style.opacity = '0'
+    }
+  })
+}
+
+function getRandomImage() {
+  let button = document.querySelector('.randomImageButton')
+  let image = document.querySelector('.randomImage img')
+
+  // Math.random() * (max - min) + min
+  button.addEventListener('click', () => {
+    let randomNumber = Math.floor(Math.random() * 8 + 1)
+
+    image.src = `images/smeshariki/smesharik${randomNumber}.webp`
+  })
+}
 
 function drawSVG() {
   let star = document.querySelector('.star')
